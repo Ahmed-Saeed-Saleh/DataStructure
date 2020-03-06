f74ecb6c75934040e75afafb4e75ecff22d02ca3
Ahmed-Saeed-Saleh <ahmedsaeedsaleh@outlook.com>
2020-03-06 15:41:12 +0200

    Update LINKEDLIST.c

--- Data structure/LINKEDLIST/src/LINKEDLIST.c
+++ Data structure/LINKEDLIST/src/LINKEDLIST.c
@@ -1,8 +1,7 @@
 /*
  ============================================================================
  Name        : linkedlist.c
- Author      : 
- Version     :
+ Author      : Ahmed Saeed
  Copyright   : Your copyright notice
  Description : linkedlist in C, Ansi-style
  ============================================================================
